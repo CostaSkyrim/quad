df63fad7137326b6e3d43f8954af8552166aa55b
kpetrout <kpetrout>
2024-06-01 17:08:55 +0300

    quadA solution

--- quadA.go
+++ quadA.go
@@ -3,8 +3,35 @@ package main
 import "fmt"
 
 func QuadA(x,y int) {
+	if x > 0 && y > 0 {
+	for row := 0; row < y; row++ {
+		for col := 0; col < x; col++ {
+			if (row == 0 && col == 0) || (row == 0 && col == x-1) || (row == y-1 && col == 0) || (row == y-1 && col == x-1) {
+				// Print corners
+				fmt.Print("o")
+			} else if row == 0 || row == y-1 {
+				// Print top or bottom edges
+				fmt.Print("-")
+			} else if col == 0 || col == x-1 {
+				// Print left or right edges
+				fmt.Print("|")
+			} else {
+				// Print inside of the rectangle
+				fmt.Print(" ")
+			}
+		}
+		// Newline after each row
+		fmt.Println()
+	}		 
+ 			
+	} else {
+		return 
+	}
+}
 
-
-
-
+func main() {
+        QuadA(5,3)
+        QuadA(5,1)
+        QuadA(1,1)
+        QuadA(1,5)
 }
