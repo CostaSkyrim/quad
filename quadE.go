7be195e31198ad8b65d457d84d9e0cd4b353e0e3
kpetrout <kpetrout>
2024-06-01 19:54:41 +0300

    quadE

---
+++ quadE.go
@@ -0,0 +1,33 @@
+package piscine
+
+import "fmt"
+
+func QuadE(x, y int) {
+        if x > 0 && y > 0 {
+                for row := 0; row < y; row++ {
+                        for col := 0; col < x; col++ {
+                                if row == 0 && col == 0 {
+                                        // Print the left corners
+                                        fmt.Print("A")
+                                } else if (row == 0 && col == x-1) || (row == y-1 && col == 0) {
+                                        // Print the right corners
+                                        fmt.Print("C")
+                                } else if row == y-1 && col == x-1 {
+                                        // Print the right corners
+                                        fmt.Print("A")
+                                } else if row == 0 || row == y-1 || col == 0 || col == x-1 {
+                                        // Print top, bottom, left and right edges
+                                        fmt.Print("B")
+                                } else {
+                                        // Print inside of the rectangle
+                                        fmt.Print(" ")
+                                }
+                        }
+                        // Newline after each row
+                        fmt.Println()
+                }
+        } else {
+                return
+        }
+}
+
---
+++ quadEtest/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "piscine"
+
+func main() {
+	piscine.QuadE(5,3)
+	piscine.QuadE(5,1)
+	piscine.QuadE(1,1)
+	piscine.QuadE(1,5)
+}
