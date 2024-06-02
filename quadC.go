ee8fd3f24098f51ff8fa23ca8a0d98df1f87cf20
saggelak <sokratisagglekopoulos@gmail.com>
2024-06-02 15:57:33 +0300

    Quad C with the right comments

--- quadC.go
+++ quadC.go
@@ -3,26 +3,27 @@ package piscine
 import "fmt"
 
 func QuadC(x, y int) {
-        if x > 0 && y > 0 {
-                for row := 0; row < y; row++ {
-                        for col := 0; col < x; col++ {
-                                if (row == 0 && col == 0) || (row == 0 && col == x-1) {
-										fmt.Print("A")
-								} else if (row == y-1 && col == 0) || (row == y-1 && col == x-1)  {	
-										fmt.Print("C")
-                                } else if row == 0 || row == y-1 || col == 0 || col == x-1 {
-                                        // Print top, bottom, left and right edges
-                                        fmt.Print("B")
-                                } else {
-                                        // Print inside of the rectangle
-                                        fmt.Print(" ")
-                                }
-                        }
-                        // Newline after each row
-                        fmt.Println()
-                }
-        } else {
-                return
-        }
+	if x > 0 && y > 0 {
+		for row := 0; row < y; row++ {
+			for col := 0; col < x; col++ {
+				if (row == 0 && col == 0) || (row == 0 && col == x-1) {
+					fmt.Print("A")
+					// Print the top corners
+				} else if row == y-1 && col == 0 || row == y-1 && col == x-1 {
+					fmt.Print("C")
+					// Print the bottom corners
+				} else if row == 0 || row == y-1 || col == 0 || col == x-1 {
+					fmt.Print("B")
+					// Print the edges
+				} else {
+					fmt.Print(" ")
+					// Print inside of the rectangle
+				}
+			}
+			// Newline after each row
+			fmt.Println()
+		}
+	} else {
+		return
+	}
 }
-
